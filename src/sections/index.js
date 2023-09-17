4f16bc261730babd74181ee091a7d9ea135ece78
Aditya Choudhary <105415827+AdityaC25@users.noreply.github.com>
2023-09-17 23:40:41 +0530

    Add files via upload

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+
+export {
+    Hero,
+    Services,
+    Subscribe,
+    SpecialOffer,
+    SuperQuality,
+    Footer,
+    CustomerReviews,
+    PopularProducts
+}
